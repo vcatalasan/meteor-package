681a9998c811aa97e23974591c14fb018d759986
Val Catalasan <val@bscmanage.com>
2015-10-13 13:14:23 -0700

    initial commit

---
+++ packages/formbuilder/formbuilder.js
@@ -0,0 +1,8 @@
+// Write your package code here!
+
+Template.formBuilder.helpers({
+    con: function(){
+        return 'XX';
+    }
+});
+
---
+++ packages/formbuilder/package.js
@@ -0,0 +1,26 @@
+Package.describe({
+    name: 'vcatalasan:formbuilder',
+    version: '0.0.1',
+    // Brief, one-line summary of the package.
+    summary: '',
+    // URL to the Git repository containing the source code for this package.
+    git: '',
+    // By default, Meteor will default to using README.md for documentation.
+    // To avoid submitting documentation, set this field to null.
+    documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+    api.versionsFrom('1.2.0.2');
+    api.use('ecmascript');
+    api.use("templating", "client");
+    api.addFiles(['formbuilder.html','formbuilder.js'], 'client');
+});
+
+Package.onTest(function(api) {
+    api.use('ecmascript');
+    api.use("templating", "client");
+    api.use('tinytest');
+    api.use('vcatalasan:formbuilder');
+    api.addFiles(['formbuilder.html','formbuilder-tests.js'], 'client');
+});
